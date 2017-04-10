ba551f89eb9a3376b5dcb65087188041229c54a7
Gulzar Ahmed <gulzarhome54@gmail.com>
2017-04-10 23:59:33 +0530

    read and write listener added

--- app/src/main/java/com/rxjava/demos/FbLiveVideoReactionDemoActivity.java
+++ app/src/main/java/com/rxjava/demos/FbLiveVideoReactionDemoActivity.java
@@ -7,8 +7,14 @@ import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.anshul.rxdownloader.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import org.reactivestreams.Subscriber;
 import org.reactivestreams.Subscription;
@@ -19,17 +25,20 @@ import io.reactivex.BackpressureStrategy;
 import io.reactivex.Flowable;
 import io.reactivex.FlowableEmitter;
 import io.reactivex.FlowableOnSubscribe;
+import io.reactivex.internal.util.ErrorMode;
 import io.reactivex.schedulers.Timed;
 
 /**
- * Created by anshul on 29/1/17.
+ * Created by anshul on 29/1/17. Modifed By Gulzar :D
  */
 
 public class FbLiveVideoReactionDemoActivity extends AppCompatActivity {
   private Subscription emoticonSubscription;
   private Subscriber subscriber;
   private final int MINIMUM_DURATION_BETWEEN_EMOTICONS = 300; // in milliseconds
+  boolean[] skip=new boolean[6];
 
+  public
   // Emoticons views.
   @BindView(R.id.like_emoticon)
   ImageView likeEmoticonButton;
@@ -47,25 +56,34 @@ public class FbLiveVideoReactionDemoActivity extends AppCompatActivity {
   @BindView(R.id.custom_view)
   EmoticonsView emoticonsView;
 
-  private Animation emoticonClickAnimation;
+  public DatabaseReference mDatabase;
 
+  private Animation emoticonClickAnimation;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     //Set the view and do all the necessary init.
     setContentView(R.layout.activity_fb_live_video_reaction_demo);
+    mDatabase= FirebaseDatabase.getInstance().getReference();
     ButterKnife.bind(this);
+
+    //SKIPS FIRST READ
+    for(int i=0;i<skip.length;i++)
+      skip[i]=true;
   }
 
   @Override
   public void onStart() {
     super.onStart();
+
     //Create an instance of FlowableOnSubscribe which will convert click events to streams
     FlowableOnSubscribe flowableOnSubscribe = new FlowableOnSubscribe() {
       @Override
       public void subscribe(final FlowableEmitter emitter) throws Exception {
         convertClickEventToStream(emitter);
+        //Reads data on Change
+        readData(emitter);
       }
     };
     //Give the backpressure strategy as BUFFER, so that the click items do not drop.
@@ -136,6 +154,8 @@ public class FbLiveVideoReactionDemoActivity extends AppCompatActivity {
       @Override
       public void onClick(View v) {
         doOnClick(likeEmoticonButton, emitter, Emoticons.LIKE);
+        fireData(Emoticons.LIKE.toString());
+
       }
     });
 
@@ -143,6 +163,7 @@ public class FbLiveVideoReactionDemoActivity extends AppCompatActivity {
       @Override
       public void onClick(View v) {
         doOnClick(loveEmoticonButton, emitter, Emoticons.LOVE);
+        fireData(Emoticons.LOVE.toString());
       }
     });
 
@@ -150,6 +171,7 @@ public class FbLiveVideoReactionDemoActivity extends AppCompatActivity {
       @Override
       public void onClick(View v) {
         doOnClick(hahaEmoticonButton, emitter, Emoticons.HAHA);
+        fireData(Emoticons.HAHA.toString());
       }
     });
 
@@ -157,6 +179,7 @@ public class FbLiveVideoReactionDemoActivity extends AppCompatActivity {
       @Override
       public void onClick(View v) {
         doOnClick(wowEmoticonButton, emitter, Emoticons.WOW);
+        fireData(Emoticons.WOW.toString());
       }
     });
 
@@ -164,6 +187,7 @@ public class FbLiveVideoReactionDemoActivity extends AppCompatActivity {
       @Override
       public void onClick(View v) {
         doOnClick(sadEmoticonButton, emitter, Emoticons.SAD);
+        fireData(Emoticons.SAD.toString());
       }
     });
 
@@ -171,6 +195,7 @@ public class FbLiveVideoReactionDemoActivity extends AppCompatActivity {
       @Override
       public void onClick(View v) {
         doOnClick(angryEmoticonButton, emitter, Emoticons.ANGRY);
+        fireData(Emoticons.ANGRY.toString());
       }
     });
   }
@@ -179,6 +204,90 @@ public class FbLiveVideoReactionDemoActivity extends AppCompatActivity {
     emoticonClickAnimation = AnimationUtils.loadAnimation(FbLiveVideoReactionDemoActivity
         .this, R.anim.emoticon_click_animation);
     view.startAnimation(emoticonClickAnimation);
-    emitter.onNext(emoticons);
+  }
+
+  private void fireData(String emoticon)
+  {
+    String key=mDatabase.child(emoticon+"").push().getKey();
+    mDatabase.child(emoticon+"").child(key).setValue(true);
+  }
+
+  private void readData(final FlowableEmitter emitter)
+  {
+
+
+    mDatabase.child(Emoticons.LIKE.toString()).addValueEventListener(new ValueEventListener() {
+      @Override
+      public void onDataChange(DataSnapshot dataSnapshot) {
+        if(!skip[0])
+        emitter.onNext(Emoticons.LIKE);
+        skip[0]=false;
+      }
+      @Override
+      public void onCancelled(DatabaseError databaseError) {
+
+      }
+    });
+    mDatabase.child(Emoticons.LOVE.toString()).addValueEventListener(new ValueEventListener() {
+      @Override
+      public void onDataChange(DataSnapshot dataSnapshot) {
+        if(!skip[1])
+        emitter.onNext(Emoticons.LOVE);
+        skip[1]=false;
+      }
+      @Override
+      public void onCancelled(DatabaseError databaseError) {
+
+      }
+    });
+    mDatabase.child(Emoticons.WOW.toString()).addValueEventListener(new ValueEventListener() {
+      @Override
+      public void onDataChange(DataSnapshot dataSnapshot) {
+        if(!skip[2])
+        emitter.onNext(Emoticons.WOW);
+        skip[2]=false;
+      }
+      @Override
+      public void onCancelled(DatabaseError databaseError) {
+
+      }
+    });
+    mDatabase.child(Emoticons.HAHA.toString()).addValueEventListener(new ValueEventListener() {
+      @Override
+      public void onDataChange(DataSnapshot dataSnapshot) {
+        if(!skip[3])
+        emitter.onNext(Emoticons.HAHA);
+        skip[3]=false;
+      }
+      @Override
+      public void onCancelled(DatabaseError databaseError) {
+
+      }
+    });
+    mDatabase.child(Emoticons.ANGRY.toString()).addValueEventListener(new ValueEventListener() {
+      @Override
+      public void onDataChange(DataSnapshot dataSnapshot) {
+        if(!skip[4])
+        emitter.onNext(Emoticons.ANGRY);
+        skip[4]=false;
+      }
+      @Override
+      public void onCancelled(DatabaseError databaseError) {
+
+      }
+    });
+    mDatabase.child(Emoticons.SAD.toString()).addValueEventListener(new ValueEventListener() {
+      @Override
+      public void onDataChange(DataSnapshot dataSnapshot) {
+        if(!skip[5])
+        emitter.onNext(Emoticons.SAD);
+        skip[5]=false;
+      }
+      @Override
+      public void onCancelled(DatabaseError databaseError) {
+
+      }
+    });
+
   }
 }
